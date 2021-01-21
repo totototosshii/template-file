e0bf44b46771ee03898192f403e148003d81429c
tosshii <sj.syj.bumo@gmail.com>
2021-01-21 13:22:49 +0900

    git init

---
+++ js/common.js
@@ -0,0 +1,10 @@
+// --------------------------------
+//             ドロワー
+// --------------------------------
+// $('.drawer_nav').on('click', function () {
+//   $(this).toggleClass('js-active');
+//   $('.').toggleClass('js-active');
+//   $('.').slideToggle(500);
+//   $('.').fadeToggle(500);
+//   $('body').toggleClass('js-noScroll');
+// });
