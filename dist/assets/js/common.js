2e498b23921fd0365ea688e3f49540764a102ea0
tosshii <sj.syj.bumo@gmail.com>
2021-04-05 21:26:26 +0900

    no message

--- dist/assets/js/common.js
+++ dist/assets/js/common.js
@@ -10,3 +10,12 @@
 //   $('.').fadeToggle(500);
 //   $('body').toggleClass('is_active');
 // });
+
+
+// iOSのブラウザバック時に強制リロード
+// 参考→https://stray-light.info/wp/ios-safari-backbutton/
+window.onpageshow = function (event) {
+  if (event.persisted) {
+    window.location.reload();
+  }
+};
